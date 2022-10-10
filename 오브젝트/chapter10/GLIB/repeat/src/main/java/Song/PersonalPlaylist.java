8068bf69d2f4dd967de251ad1a430963151d4435
WooJaewui <sooyo12@naver.com>
2022-10-10 23:00:27 +0900

    오브젝트
    10_상속과 코드 재사용

---
+++ 오브젝트/chapter10/GLIB/repeat/src/main/java/Song/PersonalPlaylist.java
@@ -0,0 +1,11 @@
+package Song;
+
+public class PersonalPlaylist extends Playlist{
+
+    public void remove(Song song) {
+        getTracks().remove(song);
+        getSingers().remove(song.getSinger());
+    }
+
+
+}
---
+++ 오브젝트/chapter10/GLIB/repeat/src/main/java/phone/RegularPhone.java
@@ -0,0 +1,22 @@
+package phone;
+
+import java.time.Duration;
+
+public class RegularPhone extends Phone {
+
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds, double taxRate) {
+        super(taxRate);
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+
+}
+
